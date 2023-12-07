ba443d2194a2ae1d7e7f2bd774aa0865e91eb728
noahwekesa <noahwekesa0@gmail.com>
2023-12-07 21:51:34 +0300

    middleware

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <button
             onClick={logOut}
             type="button"
-            className="text-white bg-blue-600 hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-red-700-600 hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Logout
           </button>
